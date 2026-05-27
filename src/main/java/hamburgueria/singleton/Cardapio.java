a40b34127f12d4c9c8666526722a31e0f05cbe73
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-27 11:27:54 -0300

    feat: CardapioList

--- src/main/java/hamburgueria/singleton/Cardapio.java
+++ src/main/java/hamburgueria/singleton/Cardapio.java
@@ -1,7 +1,28 @@
 package hamburgueria.singleton;
 
 
+import hamburgueria.model.Lanche;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Cardapio {
 
 
+    private static final Cardapio instancia = new Cardapio();
+    private final List<Lanche> lanches = new ArrayList<>();
+    
+    private Cardapio() {
+        lanches.add(new Lanche("X-Burguer", 18.90));
+        lanches.add(new Lanche("X-Bacon Duplo", 32.90));
+        lanches.add(new Lanche("X-Salada", 22.00));
+    }
+    public static Cardapio getInstancia() {
+        return instancia;
+    }
+    public List<Lanche> getLanches() {
+        return Collections.unmodifiableList(lanches); }
+
+    public void adicionarLanche(Lanche lanche) { lanches.add(lanche); }
+}
