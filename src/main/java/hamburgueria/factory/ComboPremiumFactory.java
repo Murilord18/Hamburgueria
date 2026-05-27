b47cafb05af9551bcc8ecff85cdef9bf22084ac2
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-05-27 11:36:41 -0300

    feat: Adiciona Combo Premium

---
+++ src/main/java/hamburgueria/factory/ComboPremiumFactory.java
@@ -0,0 +1,11 @@
+package hamburgueria.factory;
+
+import hamburgueria.model.Lanche;
+
+public class ComboPremiumFactory implements LancheFactory {
+    public Lanche criarLanche() {
+        return new Lanche("X-Bacon Duplo", 32.90); }
+
+    public Lanche criarAcompanhamento() {
+        return new Lanche("Batata Frita G + Onion Rings", 15.00); }
+}
