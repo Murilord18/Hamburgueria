9c7c604f8e26be593daf99de2a7b1d9c5babcc60
Murilo Schittino <146205147+Murilord18@users.noreply.github.com>
2026-06-09 17:17:34 -0300

    Casos de Testes Strategy

---
+++ src/test/java/hamburgueria/StrategyTest.java
@@ -0,0 +1,70 @@
+package hamburgueria;
+
+import hamburgueria.strategy.*;
+import hamburgueria.strategy.CarrinhoCompras;
+import hamburgueria.strategy.DescontoFidelidade;
+import hamburgueria.strategy.DescontoPromocao;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class StrategyTest {
+
+    private static final double DELTA = 0.001;
+
+    @Test
+    public void testSemDesconto() {
+        CarrinhoCompras carrinho = new CarrinhoCompras(50.0);
+        assertEquals(50.0, carrinho.calcularTotal(), DELTA);
+    }
+
+
+
+    @Test
+    public void testDescontoFidelidade15Porcento() {
+        CarrinhoCompras carrinho = new CarrinhoCompras(100.0);
+        carrinho.setDescontoStrategy(new DescontoFidelidade());
+        assertEquals(85.0, carrinho.calcularTotal(), DELTA);
+    }
+
+    @Test
+    public void testDescontoPromocao20Porcento() {
+        CarrinhoCompras carrinho = new CarrinhoCompras(100.0);
+        carrinho.setDescontoStrategy(new DescontoPromocao());
+        assertEquals(80.0, carrinho.calcularTotal(), DELTA);
+    }
+
+    @Test
+    public void testTrocarEstrategiaEmTempoDeExecucao() {
+        CarrinhoCompras carrinho = new CarrinhoCompras(200.0);
+
+        carrinho.setDescontoStrategy(new DescontoFidelidade());
+        assertEquals(170.0, carrinho.calcularTotal(), DELTA);
+
+        carrinho.setDescontoStrategy(new DescontoPromocao());
+        assertEquals(160.0, carrinho.calcularTotal(), DELTA);
+    }
+
+
+
+    @Test
+    public void testDescricaoSemDesconto() {
+        CarrinhoCompras carrinho = new CarrinhoCompras(50.0);
+        assertEquals("Sem desconto", carrinho.getDescricaoDesconto());
+    }
+
+
+
+    @Test
+    public void testDescricaoDescontoFidelidade() {
+        CarrinhoCompras carrinho = new CarrinhoCompras(50.0);
+        carrinho.setDescontoStrategy(new DescontoFidelidade());
+        assertEquals("Desconto fidelidade (15%)", carrinho.getDescricaoDesconto());
+    }
+
+    @Test
+    public void testDescricaoDescontoPromocao() {
+        CarrinhoCompras carrinho = new CarrinhoCompras(50.0);
+        carrinho.setDescontoStrategy(new DescontoPromocao());
+        assertEquals("Desconto promocao (20%)", carrinho.getDescricaoDesconto());
+    }
+}
